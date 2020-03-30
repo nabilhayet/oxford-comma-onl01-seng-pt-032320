4c16f697e4d33c9f99af6f89fd3f55b9916fc431
Nabil Hayet <nabil.bracu@gmail.com>
2020-03-30 21:13:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
+require "pry"
 def oxford_comma(array)
-
+  s=""
+  if array.length() ==1 
+    s=array.join
+  elsif array.length() ==2 
+       s=s+array.join(" and ")
+  else
+    array[-1].insert(0, "and ")
+    s=array.join(", ")
+  end 
+  return s 
 end
+
