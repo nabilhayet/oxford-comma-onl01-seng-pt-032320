cbda6111908227ef94ea07c07841ad2e96158c8c
Nabil Hayet <nabil.bracu@gmail.com>
2020-03-30 20:55:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  s=""
+  
+  if array.length() ==1 
+    s=array.join
+  else if array.length() ==2 
+       s=s+array.join(" and ")
+        
+  else
+    
+    s=s+array.join(", ")
+   
+   
+   end
+end 
+return s 
 end
+
